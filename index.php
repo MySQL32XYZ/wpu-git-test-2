0293eef062124843476a6d8e28a1ae5f1f07ead4
MySQL32XYZ <ferdinalputramaysal@gmail.com>
2024-10-03 09:16:13 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
   <h1>GIT GUD</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque quasi harum error quos odit aut repudiandae hic. Dolores tempore quisquam alias harum nemo consequatur, atque tempora mollitia velit quod.</p>
+  <ul>
+    <li>Ichi</li>
+    <li>Ni</li>
+    <li>San</li>
+  </ul>
 </body>
 
 </html>
